27b295f596c181e13ad946e891e38aa3009b7b7f
YuvaKishore <dondapatiyk@gmail.com>
2021-09-28 13:14:35 +0530

    requesting location permission

--- app/src/main/java/com/tracking/yudi/MainActivity.kt
+++ app/src/main/java/com/tracking/yudi/MainActivity.kt
@@ -1,24 +1,27 @@
 package com.tracking.yudi
 
 import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-
-// check manifests for permissions
-private val REQUIRED_PERMISSIONS = arrayOf(
-    Manifest.permission.INTERNET,
-    Manifest.permission.ACCESS_FINE_LOCATION,
-    Manifest.permission.ACCESS_COARSE_LOCATION,
-    Manifest.permission.ACCESS_NETWORK_STATE
-)
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.net.PlacesClient
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private var locationPermissionGranted: Boolean = false
+    private lateinit var locationServices: FusedLocationProviderClient
+    private lateinit var placesClient: PlacesClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,9 +29,16 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         // Get the SupportMapFragment and request notification when the map is ready to be used.
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
+
+        Places.initialize(this, BuildConfig.MAPS_API_KEY)
+        placesClient = Places.createClient(this)
+
+        //The LocationServices interface is the main entry point for Android location services.
+        locationServices = LocationServices.getFusedLocationProviderClient(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
+
         val sydney = LatLng(-33.852, 151.211)
         googleMap.addMarker(
             MarkerOptions()
@@ -36,6 +46,49 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 .title("Marker in Sydney")
         )
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        getLocationPermission()
+    }
 
+    private fun getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(
+                this.applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        locationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true
+                }
+            }
+        }
+    }
+    companion object {
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
     }
 }
