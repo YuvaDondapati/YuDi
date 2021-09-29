a2ec8597fe99dca8083a812d76d06a69502a6063
YuvaKishore <dondapatiyk@gmail.com>
2021-09-29 16:18:22 +0530

    detecting current location

--- app/src/main/java/com/tracking/yudi/MainActivity.kt
+++ app/src/main/java/com/tracking/yudi/MainActivity.kt
@@ -2,7 +2,9 @@ package com.tracking.yudi
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -18,9 +20,12 @@ import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.net.PlacesClient
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private var map: GoogleMap? = null
     private var locationPermissionGranted: Boolean = false
-    private lateinit var locationServices: FusedLocationProviderClient
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var placesClient: PlacesClient
+    private var lastKnownLocation: Location? = null
+    private val defaultLocation = LatLng(-33.8523341, 151.2106085)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,20 +39,25 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         placesClient = Places.createClient(this)
 
         //The LocationServices interface is the main entry point for Android location services.
-        locationServices = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
 
-        val sydney = LatLng(-33.852, 151.211)
-        googleMap.addMarker(
-            MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney")
-        )
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        this.map = googleMap
+//        val sydney = LatLng(-33.852, 151.211)
+//        googleMap.addMarker(
+//            MarkerOptions()
+//                .position(sydney)
+//                .title("Marker in Sydney")
+//        )
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
         getLocationPermission()
+        // Turn on the My Location layer and the related control on the map.
+//        updateLocationUI()
+
     }
+
 
     private fun getLocationPermission() {
         /*
@@ -82,13 +92,80 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.isNotEmpty() &&
-                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
                     locationPermissionGranted = true
                 }
             }
         }
+        updateLocationUI()
     }
+
+    private fun updateLocationUI() {
+        if (map == null) {
+            return
+        }
+        try {
+            if (locationPermissionGranted) {
+                map?.isMyLocationEnabled = true
+                map?.uiSettings?.isMyLocationButtonEnabled = true
+
+                // Get the current location of the device and set the position of the map.
+                getDeviceLocation()
+
+            } else {
+                map?.isMyLocationEnabled = false
+                map?.uiSettings?.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+
+    }
+
+    private fun getDeviceLocation() {
+        /*
+      * Get the best and most recent location of the device, which may be null in rare
+      * cases when a location is not available.
+      */
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+
+                locationResult.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation = task.result
+                        lastKnownLocation?.let {
+                            map?.moveCamera(
+                                CameraUpdateFactory.newLatLngZoom(
+                                    LatLng(it.latitude, it.longitude),
+                                    DEFAULT_ZOOM.toFloat()
+                                )
+                            )
+                        }
+
+                    }else{
+                        Log.d(TAG, "Current location is null. Using defaults.")
+                        Log.e(TAG, "Exception: %s", task.exception)
+                        map?.moveCamera(CameraUpdateFactory
+                            .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat()))
+                        map?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+
+    }
+
     companion object {
         private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+        private const val DEFAULT_ZOOM = 15
+        private val TAG = MainActivity::class.java.simpleName
     }
 }
